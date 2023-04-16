d6ef85663ea0c601ec2693dfbe224f2ed815e6ed
Trần Ngọc Dương <lotaynaongo@gmail.com>
2023-04-17 05:01:46 +0700

    update config app base

--- src/server.js
+++ src/server.js
@@ -3,9 +3,28 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRouters from "./route/web";
 import connectDB from "./config/connectDB";
+
 require('dotenv').config();
 
 let application = express();
+application.use(function (request, response, next) {
+
+    // Website you wish to allow to connect
+    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    response.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 application.use(bodyParser.json());
 application.use(bodyParser.urlencoded({ extended: true }));
